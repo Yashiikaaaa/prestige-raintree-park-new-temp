f1111c94690c3e78e1df507ecc2682e92351dc88
yashiikaaaa <yashika@truestate.in>
2026-02-16 20:09:41 +0530

    xyz

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -57,7 +57,7 @@ export default function Hero() {
 
           {/* Description */}
           <p className="text-[16px] md:text-lg lg:text-lg mb-8 text-gray-200 font-['Urbanist'] font-bold drop-shadow-lg">
-            Exclusive price for first 100 customers
+            Prestige Raintree Park Phase 2
           </p>
 
           {/* CTA Button */}
